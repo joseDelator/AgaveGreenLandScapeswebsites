b0f1467cf2a188aa9e356374bfbde0f9aea57ae5
joseDelator <49530849+joseDelator@users.noreply.github.com>
2023-02-26 13:10:44 -0800

    first commit

---
+++ src/pages/Photogallery.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import mainimage from '../../public/starting.jpg'
+import pagen from '../../public/protrait.jpg'
+const Photogallery = () => {
+  return (
+    <div className='w-full h-full  grid rounded-sm m-5   gap-4 md:grid-cols-3 '>
+        <div className='bg-primary card  md:col-span-2'> <img className='card w-full h-full' src={mainimage.src}>
+            </img ></div>
+        <div className='bg-primary card'><img className='card w-full h-full' src={pagen.src}/></div>
+        <div className='bg-secondary card'>3</div>
+        <div className='bg-primary card'>4</div>
+        <div className='bg-primary card'>5</div>
+        <div className='bg-primary card'>6</div>
+        <div className='bg-primary card'>7</div>
+        <div className='bg-primary card'>8</div>
+
+    </div>
+  )
+}
+
+export default Photogallery
