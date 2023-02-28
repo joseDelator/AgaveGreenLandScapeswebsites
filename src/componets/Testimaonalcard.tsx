7242ce65e424bd9724b8c5317ac4d39e7e165fec
joseDelator <fabichivas101@gmail.com>
2023-02-27 22:44:27 -0800

    second update

--- src/componets/Testimaonalcard.tsx
+++ src/componets/Testimaonalcard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-const Testimaonalcard = ({image, message, tag}) => {
+const Testimaonalcard = ({image, message,  tag}) => {
     return (
         <div className="card w-full sm:w-96 h-full bg-base-100 shadow-xl text-neutral">
         <figure><img src={image.src} alt="Shoes" /></figure>
