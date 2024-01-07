d0942752a95fd14a38c261def7c54dd667927992
joseDelator <fabichivas101@gmail.com>
2024-01-07 08:18:40 -0800

    someithing

--- next.config.js
+++ next.config.js
@@ -9,11 +9,7 @@ module.exports ={
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-    webpack: (config, { isServer }) => {
-      if (isServer) {
-        require("./scripts/sitemap-generator");
-      }
-      return config;
-    },
+   
+    
   },
 }
