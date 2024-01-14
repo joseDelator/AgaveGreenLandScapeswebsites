3ada0e1af8d1c2108644da7080e860dbf250a34f
joseDelator <fabichivas101@gmail.com>
2024-01-13 19:22:28 -0800

    closing frawering when clicking on links

--- src/componets/Navbar.tsx
+++ src/componets/Navbar.tsx
@@ -1,22 +1,26 @@
-import React from 'react'
+import React, { useId, useState } from 'react'
 import {FaPhoneAlt} from 'react-icons/fa'
-import {TbPlant2} from 'react-icons/tb'
+
 import Iconsvg from '../../public/Green Creative Growing Seed For Agriculture, Farming, Gardening Logo 2-optimized.svg'
 import Link from 'next/link'
 import Image from 'next/image'
 Image
 const Navbar = () => {
+  const [Checked, setChecked] = useState(false)
+  const toggle =()=>{
+    setChecked(!Checked) 
+  }
   return (
     <div className="navbar  bg-base-100  sticky top-0 z-50 ">
     <div className="navbar-start">
-    <Link  href="/"  className="btn btn-ghost" scroll={false}>
+    <Link  href="/"  className="btn btn-ghost" scroll={false} >
     <div className="text-s md:text-lg">Agave Green Landscapes</div>
-    <Image src={Iconsvg}  alt="Agave Green Logo"   height={35} />
+    <Image src={Iconsvg}  alt="Agave Green Logo" height={35} />
    </Link>
     </div>
     <div className="navbar-end ">
     <div className="drawer justify-end">
-        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" checked={Checked} onClick={toggle} />
         <div className="drawer-content">
           <label htmlFor="my-drawer" className="btn btn-primary drawer-button"> 
            <svg xmlns="http://www.w3.dorg/2000/svg" className="h-5 w-5"
@@ -26,31 +30,31 @@ const Navbar = () => {
         </label>
         </div> 
         <div className="drawer-side ">
-          <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+          <label htmlFor="my-drawer"  aria-label="close sidebar" className="drawer-overlay"></label>
           <ul  className="menu p-4 w-80 min-h-full bg-base-200  text-base-content">
             <li className='text-center items-center' aria-label="close sidebar">
-            <Link    href="/" scroll={false}>Agave Green Landscapes</Link>
+            <Link    href="/"  onClick={toggle} scroll={false}>Agave Green Landscapes</Link>
             </li>
-            <div className='divider divider-neutral ' ></div>
+            <div className='divider divider-neutral' ></div>
             <li>
-              <Link    href="/#Contact" scroll={false}>Contact us!</Link>
+              <Link   onClick={toggle}  href="/#Contact" scroll={false}>Contact us!</Link>
             </li>
             <div className='divider' ></div>
             <li>
-              <Link    href="/OurProcess" >Our Process</Link>
+              <Link onClick={toggle}   href="/OurProcess" >Our Process</Link>
             </li> 
-            <div className='divider '></div>
+            <div onClick={toggle} className='divider '></div>
             <li>
-              <Link href="/#testimonial"  scroll={false}>Testimonials</Link>
+              <Link onClick={toggle} href="/#testimonial"  scroll={false}>Testimonials</Link>
             </li>
             <div className='divider ' ></div>
             <li>
-              <h2 className=" flex items-center gap-4 px-1.5"><span className="">
+              <h2  onClick={toggle} className=" flex items-center gap-4 px-1.5"><span className="">
            </span> Services</h2> 
            <ul>
-            <li><Link  href="/LandscapeMangment" className="group p-3" > <span>Landscape Management</span> </Link></li>
-            <li><Link href="/LandScapeDesign" className="group p-3"> <span>LandScape Design</span> </Link></li>
-            <li><Link href="/LandscapeContruction" className="group p-3"> <span>Landscape Contruction</span> </Link></li>
+            <li><Link onClick={toggle}  href="/LandscapeMangment" className="group p-3" > <span>Landscape Management</span> </Link></li>
+            <li><Link onClick={toggle} href="/LandScapeDesign" className="group p-3"> <span>LandScape Design</span> </Link></li>
+            <li><Link onClick={toggle} href="/LandscapeContruction" className="group p-3"> <span>Landscape Contruction</span> </Link></li>
             </ul> 
             </li>
             
